9dc14d384b51bd99ace3cc31da6e97f0fa502812
1carloso1 <olveramayor.carlos@gmail.com>
2023-03-15 15:49:04 -0600

    transformador_txt

--- src/transformers/txt_transformer.py
+++ src/transformers/txt_transformer.py
@@ -23,19 +23,36 @@ class TXTTransformer(luigi.Task):
         result = []
         for file in self.input():
             with file.open() as txt_file:
-                # Leer el archivo de texto y transformar los datos
-                header = txt_file.readline().strip().split(',')
                 for line in txt_file:
-                    parts = line.strip().split(',')
-                    result.append({
-                        'description': parts[2],
-                        'quantity': int(parts[3]),
-                        'price': float(parts[5]),
-                        'total': float(float(parts[3])) * float(parts[5]),
-                        'invoice': int(parts[0]),
-                        'provider': int(parts[6]),
-                        'country': parts[7].strip()
-                    })
+                    # Split the line by comma
+                    line_parts = line.strip().split(',')
+
+                    # Check if the line has the expected number of parts
+                    if len(line_parts) != 8:
+                        continue
+
+                    # Extract the values and format them
+                    fatura_num = line_parts[0].strip()
+                    inventario_cod = line_parts[1].strip()
+                    descricao = line_parts[2].strip()
+                    montante = line_parts[3].strip()
+                    data_fatura = line_parts[4].strip()
+                    preco_unit = line_parts[5].strip()
+                    cliente_id = line_parts[6].strip()
+                    pais = line_parts[7].strip()
+
+                    # Create the result dictionary
+                    result.append(
+                        {
+                            "description": descricao,
+                            "quantity": montante,
+                            "price": preco_unit,
+                            "total": float(montante) * float(preco_unit),
+                            "invoice": fatura_num,
+                            "provider": cliente_id,
+                            "country": pais
+                        }
+                    )
            
         with self.output().open('w') as out:
             out.write(json.dumps(result, indent=4))
